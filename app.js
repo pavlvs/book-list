6e504bcfaea1084877683f4dfc3c45acc87e0d40
pavlvs <pavlvsx@gmail.com>
2018-01-25 07:00:54 +0100

    add validation and alert messages

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // Book Constructor
-function Book(title, author, isbn) { 
+function Book(title, author, isbn) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
- }
+}
 
 
 // Ui Constructor
 function UI() {}
 
 // Add book to list
-UI.prototype.addBookToList = function (book) {  
+UI.prototype.addBookToList = function (book) {
     const list = document.getElementById('book-list');
     // create tr element
     const row = document.createElement('tr');
@@ -25,10 +25,30 @@ UI.prototype.addBookToList = function (book) {
     list.appendChild(row);
 }
 // clear fields
-UI.prototype.clearFields = function() {
-       document.getElementById('title').value = '' ;
-       document.getElementById('author').value = '' ;
-       document.getElementById('isbn').value = '' ;
+UI.prototype.clearFields = function () {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+// show alert
+UI.prototype.showAlert = function(message, className) {
+    // create div
+    const div = document.createElement('div');
+    //Add message text
+    div.appendChild(document.createTextNode(message));
+    //add classes to div
+    div.className = `alert ${className}`;
+    // get the parent container
+    const container = document.querySelector('.container');
+    // get the form
+    const form = document.getElementById('book-form');
+    // Insert alert
+    container.insertBefore(div, form);
+    // clear alert after 3 seconds
+    setTimeout(function() {
+        document.querySelector('.alert').remove();}, 3000);
+    
 }
 
 // Event listeners
@@ -37,18 +57,27 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     console.log('test');
     // get form values
     const title = document.getElementById('title').value,
-            author = document.getElementById('author').value,
-            isbn = document.getElementById('isbn').value;
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value;
     // instantiate a bbok
     const book = new Book(title, author, isbn);
-    
+
     const ui = new UI();
 
-    // add book to list
-    ui.addBookToList(book);
+    // validation
+    if (title === '' || author === '' || isbn === '') {
+        // error alert
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        // add book to list
+        ui.addBookToList(book);
 
-    // clear fields
-    ui.clearFields();
+        ui.showAlert('Book added successfully', 'success');
+
+        // clear fields
+        ui.clearFields();
+    }
+
 
     e.preventDefault();
-  });
+});
